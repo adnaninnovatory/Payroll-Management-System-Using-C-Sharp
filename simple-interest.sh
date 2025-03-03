9b89ba54f3db9c7ba6c95ada4a6891ebd7f7b41b
Adnan Nazir <141548121+adnaninnovatory@users.noreply.github.com>
2025-03-03 09:56:26 +0500

    Delete simple-interest.sh

--- simple-interest.sh
+++
@@ -1,9 +0,0 @@
-A simple Bash script to calculate simple interest:
-
-```sh
-#!/bin/bash
-read -p "Enter Principal Amount: " p
-read -p "Enter Rate of Interest: " r
-read -p "Enter Time (in years): " t
-si=$(echo "$p * $r * $t / 100" | bc)
-echo "Simple Interest: $si"
